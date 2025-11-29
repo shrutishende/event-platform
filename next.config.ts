a76ef33c029c1c6d6046ea43a7c47dbafba91c84
Shruti Shende <shrutishende11@gmail.com>
2025-11-29 23:20:32 +0530

    Added homepage.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    /* config options here */
+    // reactCompiler: true,
+
+    // experimental: {
+    //     turbopackFileSystemCacheForDev: true,
+    // },
 };
 
 export default nextConfig;
