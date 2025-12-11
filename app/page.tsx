6c633eee3ecaadafb81060ba89ad4899ab04fcbe
Shruti Shende <shrutishende11@gmail.com>
2025-12-11 14:40:02 +0530

    Fix build issues.

--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,16 @@ import EventCard from "@/components/EventCard";
 import ExploreBtn from "@/components/ExploreBtn";
 import { IEvent } from "@/database";
 import { cacheLife } from "next/cache";
+import events from "@/lib/constants";
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
 const Home = async () => {
     "use cache"
     cacheLife("minutes")
-    const response = await fetch(`${BASE_URL}/api/events`);
+    // const response = await fetch(`${BASE_URL}/api/events`);
 
-    const { events } = await response.json();
+    // const { events } = await response.json();
     
     return (
         <section>
